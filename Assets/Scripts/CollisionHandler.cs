94e049e5ccd4e3c72f8579837cafa05acb592064
zeivhann <alexsilcott@gmail.com>
2018-05-31 22:55:24 -0400

    Added collisions and some game objects to interact with.

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Player collided with " + collision.ToString());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+        print("Player dying");
+        SendMessage("DisableControls");
+    }
+}
--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,22 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class MusicPlayer : MonoBehaviour {
+public class MusicPlayer : MonoBehaviour
+{
 
-    private void Awake()
+    void Awake()
     {
         GameObject.DontDestroyOnLoad(this);
     }
 
-    // Use this for initialization
-    void Start () {
-        Invoke("LoadFirstScene", 12f);
-	}
-	
-	void LoadFirstScene ()
-    {
-        SceneManager.LoadScene(1);
-    }
+
 }
